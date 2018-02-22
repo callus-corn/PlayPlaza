e9885cd10a030066d9f248bed2f9fd2c5c129a23
collus-corn <mtfm_ymzk@yahoo.co.jp>
2018-02-22 22:20:40 +0000

    add admin

--- account/models.py
+++ account/models.py
@@ -1,30 +1,53 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-class User(AbstractBaseUser):
-    name = models.TextField()
-    objects = MyUserManager()
-    USERNAME_FIELD = 'name'
-
-    def __str__(self):
-        return self.name
 
 class UserManager(BaseUserManager):
-    def create_user(self, name, password=None):
-        if not name:
+    def create_user(self, email, name, password=None):
+        if not email:
             raise ValueError('Error')
         user = self.model(
-            name = name,
+            email=self.normalize_email(email),
+            name=name,
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, name, password):
+    def create_superuser(self, email, name, password):
         user = self.create_user(
+            email,
             name,
             password,
         )
-        user.is_admin = Ture
+        user.is_admin = True
         user.save(using=self._db)
         return user
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    name = models.TextField()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
