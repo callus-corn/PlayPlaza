f93c6857c95a22c14aabf5343bb8ba59890a72f3
collus-corn <mtfm_ymzk@yahoo.co.jp>
2018-02-22 21:41:17 +0000

    [Add] create user

--- register/views.py
+++ register/views.py
@@ -1,1 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+
+from account.models import User
+from .forms import UserCreationForm
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = "form.html"
+    success_url = "/"
